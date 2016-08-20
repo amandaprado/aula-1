a7178134f247f0f3c7cc2ef6c4f51452cd77dc8b
AmandaPrado <amandalemosprado@hotmail.com>
2016-08-20 17:56:14 -0300

    separando arquivos

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Hello World!
+<?php
+  $variavel = "Hello World";
+ ?>
+<!DOCTYPE html>
+<html>
+  <?php
+    include "head.php";
+   ?>
+  <body>
+    <?php
+      echo $variavel;
+     ?>
+
+  </body>
+</html>
